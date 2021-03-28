ae3801a43f8c824d680a189e225cdb81ee9cd508
Vidrochka <Evgenuy1605@yandex.ru>
2021-03-28 21:04:47 +0300

    Check configuration manager

--- main.cpp
+++ main.cpp
@@ -1,22 +1,33 @@
 #include <string>
 #include <iostream>
 #include "Utils/Utils.hpp"
+#include <locale.h>
 
 int main()
 {
     try
     {
+        setlocale(LC_ALL, "");
         at::utils::logger::ATLogger::init_base_logger();
-        at::utils::logger::ATLogger::create_section("test", new at::utils::logger::strategy::DefaultFileLogStrategy{"./text1.log", 6});
-        at::utils::logger::interface::ILogger *logger = new at::utils::logger::ATLogger("test");
-        LOG_ARGS4("1", "2", "3", "4")
-        logger->add_strategy(new at::utils::logger::strategy::DefaultFileLogStrategy{"./test2.log", 5});
-        logger->log_debug("test 1");
-        ((at::utils::logger::ATLogger *)logger)->b_log_debug("b test 1 дда 佐藤 幹夫");
-        logger->log_error("test 2");
+        at::utils::logger::ATLogger::create_section(L"test", new at::utils::logger::strategy::DefaultFileLogStrategy{L"./text1.log", 6});
+        at::utils::logger::interface::ILogger *logger = new at::utils::logger::ATLogger(L"test");
+        LOG_ARGS4(L"1", L"2", L"3", L"4")
+        logger->add_strategy(new at::utils::logger::strategy::DefaultFileLogStrategy{L"./test2.log", 5});
+        logger->log_debug(L"test 1");
+        ((at::utils::logger::ATLogger *)logger)->b_log_debug(L"b test 1 дда 佐藤 幹夫 ");
+        logger->log_error(L"test 2");
+
+        at::utils::config_manager::source::interface::IConfigSourceInterface *source = new at::utils::config_manager::source::DefaultFileConfigSource(L"./test.toml");
+        at::utils::config_manager::parsing_strategy::interface::IParsingStrategy *strategy = new at::utils::config_manager::parsing_strategy::TomlParsingStrategy();
+        auto config = at::utils::config_manager::ConfigManager::get_config(source, strategy);
+
+        logger->log_info(config->get_section(L"default")->get_value(L"test"));
+
+        delete source;
+        delete strategy;
         //logger.flush();
-        logger->log_fatal("test 3", 2, LINE_STAMP);
-        logger->log_error("test 4");
+        logger->log_fatal(L"test 3", 2, LINE_STAMP);
+        logger->log_error(L"test 4");
     }
     catch (std::string err_msg)
     {
